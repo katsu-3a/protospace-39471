4707d00e3f2b9343768e598251a588d4b9248141
katsu-3a <katsuya9001@gmail.com>
2023-07-13 17:06:16 +0900

    fix1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!, only: [:new, :destroy]
+  before_action :authenticate_user!, only: [:new, :destroy, :create, :edit, :update]
+
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
@@ -9,5 +11,10 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource_or_scope)
     root_path # ←redirect先にしたいpathを自分で書く
   end
-  
+
+  def require_login
+    unless user_signed_in?
+      redirect_to new_user_session_path, alert: "ログインしてください"
+    end
+  end
 end
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
   before_action :set_prototype, only: [:edit, :show]
-
+  before_action :require_login, only: [:new, :edit]
   def index
     @prototypes = Prototype.all
   end
